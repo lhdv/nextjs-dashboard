9c75e7236c1aea796bcb4753e516af261b234eee
LH <lh@lhdv.net>
2024-01-04 12:43:46 -0300

    Add form validation on update invoice
    - Validate all fileds on form using Zod and useFormState hook

--- app/lib/actions.tsx
+++ app/lib/actions.tsx
@@ -63,12 +63,24 @@ export async function createInvoice(prevState: State, formData: FormData) {
     redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const {customerId, amount, status} = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+    
+    // Validate form fields using Zod
+    const validatedFields = UpdateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
+
+    // Perform form fields validations
+    if (!validatedFields.success) {
+        return{
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing fields. Failed to update invoice.'
+        };
+    }
+
+    const {customerId, amount, status} = validatedFields.data;
 
     const amountInCents = amount * 100;
 
